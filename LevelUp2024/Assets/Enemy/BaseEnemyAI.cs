4ee3b67af46284d1da84e4e523f1a56551bc692d
KhadijaI <khadijaiqbaldj@gmail.com>
2024-11-03 02:08:38 +0500

    .

--- LevelUp2024/Assets/Enemy/BaseEnemyAI.cs
+++ LevelUp2024/Assets/Enemy/BaseEnemyAI.cs
@@ -1,19 +1,9 @@
 using UnityEngine;
 
-public enum TypeOfAI
-{
-    StationaryAI,
-    ChasingAI,
-    NonChasingAI,
-    TeleportingAI,
-    RangedAI,
-    CowardAI
-
-}
 
 public abstract class BaseEnemyAI : MonoBehaviour
 {
-    public TypeOfAI TypeOfAI; // we can select what type of AI we want the enemy to be
+   
     public Transform player;
     public float health;
     public float lineOfSight; // from how far the AI will detect the player
